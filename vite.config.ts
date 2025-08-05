7653ee4d90504cf21a149920a0b3cda09fabd52d
echoedinvoker <echoedinvoker@gmail.com>
2025-08-05 10:48:59 +0000

    add medi page

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import crypto from 'crypto'
+
+if (!crypto.hash) {
+  crypto.hash = function(algorithm, data) {
+    return crypto.createHash(algorithm).update(data).digest()
+  }
+}
 
 // https://vite.dev/config/
 export default defineConfig({
