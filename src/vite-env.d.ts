b5b6c9a373153ed0bd9489df7001ae8965acf910
echoedinvoker <echoedinvoker@gmail.com>
2025-08-05 10:46:06 +0000

    add medi page

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+//
+// 為 .vue 檔案宣告模組
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
